1c9b01b65a48ee80f2212b4058e2dd38c93a57d1
viiic98 <xathovic@gmail.com>
2019-12-12 21:05:13 +0000

    Remake

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-sys = __import__("sys")
+if __name__ == "__main__":
+    import sys
+    args = sys.argv
+    len = len(args)
 
-args = sys.argv
-len = len(args)
-
-if len == 1:
-    print("{:d} arguments.".format(len - 1))
-elif len == 2:
-    print("{:d} argument:".format(len - 1))
-else:
-    print("{:d} arguments:".format(len - 1))
-for i in range(1, len):
-    print("{:d}: {:s}".format(i, args[i]))
+    if len == 1:
+        print("{:d} arguments.".format(len - 1))
+    else:
+        if len == 2:
+            print("{:d} argument:".format(len - 1))
+        else:
+            print("{:d} arguments:".format(len - 1))
+        for i in range(1, len):
+            print("{:d}: {:s}".format(i, args[i]))
