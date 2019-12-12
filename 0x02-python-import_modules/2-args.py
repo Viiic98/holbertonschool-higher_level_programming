a90fd2e4d7d18787592d8995c9458a8d8a03b864
viiic98 <xathovic@gmail.com>
2019-12-12 20:55:22 +0000

    Fix the format

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,10 @@ args = sys.argv
 len = len(args)
 
 if len == 1:
-    print("0 arguments.")
+    print("{:d} arguments.".format(len - 1))
 elif len == 2:
-    print("1 argument:")
+    print("{:d} argument:".format(len - 1))
 else:
     print("{:d} arguments:".format(len - 1))
 for i in range(1, len):
-    print("{:d}: {}".format(i, args[i]))
+    print("{:d}: {:s}".format(i, args[i]))
