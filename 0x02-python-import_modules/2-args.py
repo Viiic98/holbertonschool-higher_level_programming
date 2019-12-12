ee130f075f370d3ab2b8db0593527230a58e4d72
viiic98 <xathovic@gmail.com>
2019-12-12 20:52:19 +0000

    Add conditions

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,12 @@ sys = __import__("sys")
 
 args = sys.argv
 len = len(args)
-print("{:d} arguments:".format(len - 1))
+
+if len == 1:
+    print("0 arguments.")
+elif len == 2:
+    print("1 argument:")
+else:
+    print("{:d} arguments:".format(len - 1))
 for i in range(1, len):
     print("{:d}: {}".format(i, args[i]))
