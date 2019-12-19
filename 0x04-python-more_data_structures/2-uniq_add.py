a931b92999e804b3bf12d26f7476ee6e7cc5a13b
viiic98 <xathovic@gmail.com>
2019-12-19 17:32:52 +0000

    Fix and add a loop

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,6 +3,9 @@ def uniq_add(my_list=[]):
     sum = 0
     for i in my_list:
         c = my_list.count(i)
-        if c == 1:
-            sum += i
+        while c > 1:
+            my_list.remove(i)
+            c = my_list.count(i)
+    for i in my_list:
+        sum += i
     return sum
