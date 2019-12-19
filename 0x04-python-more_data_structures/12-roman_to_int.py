c1e70ae27a54cd1714bcbc7371b901df6d3ef6b5
viiic98 <xathovic@gmail.com>
2019-12-19 21:35:16 +0000

    Add some conditions

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,38 +1,68 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    n, l = 0, len(roman_string)
+    n, l, m = 0, len(roman_string), 0
+
     if roman_string:
         for i in range(l):
             if roman_string[i] == 'I':
+                if m == 3:
+                    return 0
                 if i + 1 < l:
                     if roman_string[i + 1] != 'I':
                         n -= 1
+                        m = 0
                     else:
                         n += 1
+                        m += 1
                 else:
                     n += 1
             elif roman_string[i] == 'V':
-                n += 5
+                if i + 1 < l:
+                    if roman_string[i + 1] != 'V':
+                        n += 5
+                    else:
+                        return 0
+                else:
+                    n += 5
             elif roman_string[i] == 'X':
+                if m == 3:
+                    return 0
                 if i + 1 < l:
                     if roman_string[i + 1] == 'L':
                         n -= 10
+                        m = 0
                     elif roman_string[i + 1] == 'C':
                         n -= 10
+                        m = 0
+                    elif roman_string[i + 1] != 'X':
+                        n += 10
+                        m = 0
                     else:
                         n += 10
+                        m += 1
                 else:
                     n += 10
             elif roman_string[i] == 'L':
-                n += 50
+                if i + 1 < l:
+                    if roman_string[i + 1] != 'l':
+                        n += 50
+                    else:
+                        return 0
+                else:
+                    n += 50
             elif roman_string[i] == 'C':
+                if m == 3:
+                    return 0
                 if i + 1 < l:
                     if roman_string[i + 1] == 'D':
                         n -= 100
+                        m = 0
                     elif roman_string[i + 1] == 'M':
                         n -= 100
+                        m = 0
                     else:
                         n += 100
+                        m += 1
                 else:
                     n += 100
             elif roman_string[i] == 'D':
