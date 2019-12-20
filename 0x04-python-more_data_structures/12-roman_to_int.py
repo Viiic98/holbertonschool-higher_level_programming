9a65aa5a654a0dbc730a601735b9932711e8c4ba
Victor Arteaga <xathovic@gmail.com>
2019-12-20 14:41:06 +0000

    Add string condition

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,8 @@ def roman_to_int(roman_string):
     rom_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sum = 0
 
+    if type(roman_string) is not str or (roman_string) is None:
+        return 0
     for i in range(len(roman_string)):
         non = 0
         l = roman_string[i]
