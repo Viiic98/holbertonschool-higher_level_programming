c333a7d3ca512e9164b4e242ce7426c5d7a9c80e
viiic98 <xathovic@gmail.com>
2020-01-08 17:36:11 +0000

    Remake all function

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        if x == 0 or len(my_list) == 0:
-            return 0
-        n = 0
-        for i in my_list:
-            if x == 0:
-                return n
-            print(i, end="")
-            n += 1
-            x -= 1
+    n = 0
+    for i in range(x):
+        if x == 0:
+            break
+        try:
+            print("{}".format(my_list[i]), end="")
+        except IndexError:
+            break
+        n += 1
+        x -= 1
+    if n > 0:
         print("")
-        return n
-    except:
-        print("Unexpected error printing the list")
+    return n
