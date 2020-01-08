350ad804f68cf2c64408203baf7fd0cc8937d008
viiic98 <xathovic@gmail.com>
2020-01-08 18:05:03 +0000

    Fix the error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,14 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     n = 0
     for i in range(x):
-        if x == 0:
-            break
         try:
             print("{}".format(my_list[i]), end="")
+            n += 1
         except IndexError:
             break
-        n += 1
-        x -= 1
     if n > 0:
         print("")
     return n
