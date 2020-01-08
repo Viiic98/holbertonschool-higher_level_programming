6c233d5040ebfacc5c482e3c3d05a58165fa49d1
viiic98 <xathovic@gmail.com>
2020-01-08 18:11:41 +0000

    Remove a space

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,6 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
         except IndexError:
             raise IndexError("list out of range")
         except:
-            continue 
+            continue
     print("")
     return n
