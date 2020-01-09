8ee01001e955e30abb04f726ece4cb1b654c6067
Viiic98 <xathovic@gmail.com>
2020-01-09 18:07:40 -0500

    Add comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+""" Square """
+
+
 class Square:
+    """ Define class Square """
     def __init__(self, size=0):
+        """ Initialize Atributtes
+        Args:
+            Size: Size that is initialiced in 0
+        """
         self.__size = size
