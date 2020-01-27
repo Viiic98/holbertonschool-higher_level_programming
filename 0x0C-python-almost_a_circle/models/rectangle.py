6ebb853cc1533d8316bf74e13bd36f22b8157bb1
Viiic98 <xathovic@gmail.com>
2020-01-27 18:26:44 +0000

    Add comments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,10 +14,20 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """ Width's getter
+            Return:
+                    private width
+         """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ Width's setter
+            Arguments:
+                    @value: Must be and integer > 0
+            Return:
+                    Nothing, just set the value of width
+        """
         if type(value) != int:
             raise TypeError('width must be an integer')
         elif value <= 0:
@@ -27,10 +37,20 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """ Height's getter
+            Return:
+                    private height
+         """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ Height's setter
+            Arguments:
+                    @value: Must be and integer > 0
+            Return:
+                    Nothing, just set the value of Height
+        """
         if type(value) != int:
             raise TypeError('height must be an integer')
         elif value <= 0:
@@ -40,10 +60,20 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """ X's getter
+            Return:
+                    private X
+         """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """ X's setter
+            Arguments:
+                    @value: Must be and integer > 0
+            Return:
+                    Nothing, just set the value of X
+        """
         if type(value) != int:
             raise TypeError('x must be an integer')
         elif value < 0:
@@ -53,10 +83,20 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """ Y's getter
+            Return:
+                    private Y
+         """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """ Y's setter
+            Arguments:
+                    @value: Must be and integer > 0
+            Return:
+                    Nothing, just set the value of Y
+        """
         if type(value) != int:
             raise TypeError('y must be an integer')
         elif value < 0:
@@ -65,9 +105,17 @@ class Rectangle(Base):
             self.__y = value
 
     def area(self):
+        """ Method that find the rectangle's area
+            Return:
+                    Rectangle's area
+        """
         return self.width * self.height
 
     def display(self):
+        """ Method that displays a rectangle
+            Return:
+                    Print in stdout a rectangle
+        """
         for i in range(self.y):
             print()
         for i in range(self.height):
@@ -77,11 +125,22 @@ class Rectangle(Base):
             print()
 
     def __str__(self):
+        """ Str method defined
+
+            What's going to happen when call the class
+            with str or just print
+        """
         s = "[Rectangle] ("+str(self.id)+") "+str(self.x)+"/"+str(self.y)
         s += " - "+str(self.width)+"/"+str(self.height)
         return s
 
     def update(self, *args, **kwargs):
+        """ Method that update the information of a rectangle
+
+            Arguments:
+                    @args: aguments
+                    @kwargs: specific keys and it's value
+        """
         i = 0
         if args:
             for arg in args:
@@ -103,6 +162,10 @@ class Rectangle(Base):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """ Dictionary method
+            Return:
+                    a dictionary that show every key and value
+        """
         dic = {'x': self.__x, 'y': self.__y, 'id': self.id,
                'height': self.__height, 'width': self.__width}
         return dic
