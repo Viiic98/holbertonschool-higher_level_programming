3700c3317de1d0c3fb5ea131d3ce234f740c2590
Viiic98 <xathovic@gmail.com>
2020-01-28 00:34:24 +0000

    Add documentation

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,38 +6,43 @@ from models.square import Square
 
 
 class Base_Test(unittest.TestCase):
+    """ Class that test all """
+
     def test_rec_0(self):
+        """ Raise type error """
         with self.assertRaises(TypeError):
             r = Rectangle(1, "2")
 
     def test_rec_1(self):
+        """ Raise type error """
         with self.assertRaises(TypeError):
             r = Rectangle("1", 2)
 
     def test_rec_2(self):
+        """ Raise value error """
         with self.assertRaises(ValueError):
             r = Rectangle(1, 0)
 
     def test_rec_3(self):
+        """ Raise value error """
         with self.assertRaises(ValueError):
             r = Rectangle(0, 1)
 
     def test_rec_4(self):
+        """ Raise value error """
         with self.assertRaises(ValueError):
             r = Rectangle(1, 1, -1, 1)
 
     def test_rec_5(self):
+        """ Raise value error """
         with self.assertRaises(ValueError):
             r = Rectangle(1, 1, 1, -1)
 
     def test_rec_6(self):
+        """ Area tests """
         r = Rectangle(3, 2)
         self.assertEqual(r.area(), 6)
         r = Rectangle(5, 5)
         self.assertEqual(r.area(), 25)
         r = Rectangle(9, 8)
         self.assertEqual(r.area(), 72)
-
-    def test_rec_7(self):
-        r = Rectangle(4, 6)
-        self.assertEqual(r.display(), "####\n####\n####\n####\n")
