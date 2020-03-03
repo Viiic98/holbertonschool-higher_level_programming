4b672f4d2a175c9d8f643abab4bbf3bd63333b9d
viiic98 <xathovic@gmail.com>
2020-03-03 18:14:33 +0000

    Add task that create a DB and user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Create a new DB and User
+-- Give select permission to user
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
