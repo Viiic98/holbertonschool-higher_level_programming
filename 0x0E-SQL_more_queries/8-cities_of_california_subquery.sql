261bd8d3fed638a8fee059403ad41601460c9e5b
viiic98 <xathovic@gmail.com>
2020-03-03 20:08:05 +0000

    Add order

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -5,4 +5,5 @@ FROM hbtn_0d_usa.cities
 WHERE state_id = (
       	       SELECT id 
 	       FROM hbtn_0d_usa.states 
-	       WHERE name = "California");
+	       WHERE name = "California")
+ORDER BY id;
