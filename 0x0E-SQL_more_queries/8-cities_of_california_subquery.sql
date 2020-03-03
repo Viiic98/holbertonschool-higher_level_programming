6365f903c6aa14d8e7989e263e319d9b977d0906
viiic98 <xathovic@gmail.com>
2020-03-03 20:20:19 +0000

    Add a comment and remove identation

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,6 @@
 -- List all cities that can be found in California
---
+-- Listed in ascending order
 USE `hbtn_0d_usa`;
-SELECT cities.id 'id', cities.name 'name'
-FROM cities
-WHERE state_id = (
-      	       SELECT states.id 'id' 
-	       FROM states 
-	       WHERE states.name = 'California')
+SELECT cities.id 'id', cities.name 'name' FROM cities 
+WHERE state_id = (SELECT states.id 'id' FROM states WHERE states.name = 'California')
 ORDER BY cities.id ASC;
