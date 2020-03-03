ebe6a7f27e92de56e4e311046759de0bd3c3c725
viiic98 <xathovic@gmail.com>
2020-03-03 20:29:11 +0000

    Change the way to do it

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- List all cities that can be found in California
 -- Listed in ascending order
-USE `hbtn_0d_usa`;
-SELECT cities.id 'id', cities.name 'name' FROM cities 
-WHERE state_id = (SELECT states.id 'id' FROM states WHERE states.name = 'California')
-ORDER BY cities.id ASC;
+SELECT cities.id, cities.name
+FROM states, cities
+WHERE states.id = cities.state_id
+AND states.name = 'California';
