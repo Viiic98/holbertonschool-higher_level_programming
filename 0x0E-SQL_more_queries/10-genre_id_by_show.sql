92751dae509032684a8eec1d14963adf63e0616c
viiic98 <xathovic@gmail.com>
2020-03-04 00:40:43 +0000

    Fix output

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,9 +1,9 @@
 -- From an imported table
 -- Display the shows that have at least one genre linked
-SELECT tv_shows.title 'title', tv_show_genres.genre_id 'id'
+SELECT tv_shows.title 'title', tv_show_genres.genre_id
 FROM tv_show_genres
 INNER JOIN tv_shows
       ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
       ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY title, id ASC;
+ORDER BY title, tv_show_genres.genre_id ASC;
