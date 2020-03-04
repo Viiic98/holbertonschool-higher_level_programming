d89b67fe5856918a67c343937b792fdb2b32fa33
viiic98 <xathovic@gmail.com>
2020-03-04 00:35:36 +0000

    Fix with inner join

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,9 @@
 -- From an imported table
 -- Display the shows that have at least one genre linked
 SELECT tv_shows.title 'title', tv_show_genres.genre_id 'id'
-FROM tv_shows, tv_show_genres 
-WHERE tv_show_genres.show_id = tv_shows.id
+FROM tv_show_genres
+INNER JOIN tv_shows
+      ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+      ON tv_genres.id = tv_show_genres.show_id
 ORDER BY title, id ASC;
