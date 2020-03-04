004155829d8833e480db77d98c1804bfe518d068
viiic98 <xathovic@gmail.com>
2020-03-04 00:38:42 +0000

    Fix

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,5 +5,5 @@ FROM tv_show_genres
 INNER JOIN tv_shows
       ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
-      ON tv_genres.id = tv_show_genres.show_id
+      ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY title, id ASC;
