cf2c4a82d9b7aa0d89fc7b24c8c977b4698ac895
viiic98 <xathovic@gmail.com>
2020-03-04 01:50:46 +0000

    Fix, only one select

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,9 +3,8 @@
 SELECT tv_genres.name
 FROM tv_genres 
 INNER JOIN tv_show_genres 
-       ON tv_show_genres.genre_id = tv_genres.id 
-WHERE tv_show_genres.show_id = (
-SELECT tv_shows.id 
-FROM tv_shows 
-WHERE tv_shows.title = 'Dexter')
-ORDER BY tv_genres.name ASC
+      ON tv_show_genres.genre_id = tv_genres.id 
+INNER JOIN tv_shows
+      ON tv_shows.title = 'Dexter'
+      AND tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_genres.name ASC;
