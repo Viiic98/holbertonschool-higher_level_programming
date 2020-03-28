6e2179ad9bca9e801629c4b7a287b5c3fbf12b6c
Viiic98 <xathovic@gmail.com>
2020-03-28 18:34:44 +0000

    Fix query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                            passwd=password, db=db_name, charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states\
-                 WHERE name LIKE 'N%'\
+                 WHERE name LIKE BYNARY 'N%'\
                  ORDER BY states.id")
     query_row = cur.fetchall()
     if query_row:
