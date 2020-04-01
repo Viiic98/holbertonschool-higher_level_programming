a74eb95ddc1a15ff763713fed4280027e8392d86
Viiic98 <xathovic@gmail.com>
2020-04-01 02:53:01 +0000

    Add task that list a relationship

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" print all the states with the cities associated """
+
+
+import sys
+from relationship_state import State
+from relationship_city import City, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    records = session.query(State).all()
+    for row in records:
+        print("{}: {}".format(row.name, row.id))
+        # go through the list of cities that have the relationship
+        for city in row.cities:
+            print("\t{}: {}".format(city.id, city.name))
+    session.close()
