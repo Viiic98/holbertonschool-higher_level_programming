2be1949ffc3ce2fac874a89632a48c4115436540
Viiic98 <xathovic@gmail.com>
2020-05-12 18:15:52 -0500

    Add task

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+const URL = process.argv[2];
+const file = process.argv[3];
+
+request(URL, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  // Parse string body response to an object
+  if (response.statusCode < 400) {
+    // Write in the file
+    fs.writeFile(file, body, (err) => {
+      if (err) throw err;
+    });
+  }
+});
