b1fafb39447907f852b4cc9e9acb91cfee7cdf81
Viiic98 <xathovic@gmail.com>
2020-05-12 18:40:26 -0500

    Fix error code

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,7 @@ const request = require('request');
 const URL = process.argv[2];
 request(URL, function (error, response, body) {
   if (error) {
-    console.error(error);
+    console.log('code:', response.statusCode);
   }
   // Parse string body response to an object
   if (response.statusCode === 200) {
