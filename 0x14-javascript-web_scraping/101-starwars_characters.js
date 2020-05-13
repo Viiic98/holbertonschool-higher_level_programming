cb0e2a83d08cb2f5837893289031997a8d1eeace
Viiic98 <xathovic@gmail.com>
2020-05-13 10:43:45 -0500

    Fix

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -11,17 +11,21 @@ request(URL, function (error, response, body) {
   if (response.statusCode === 200) {
     const obj = JSON.parse(body);
     const characters = obj.characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], function (error, response, body) {
-        if (error) {
-          console.error(error);
-        }
-        // Parse string body response to an object
-        if (response.statusCode === 200) {
-          const char = JSON.parse(body);
-          console.log(char.name);
-        }
-      });
-    }
+    charsInFilm(characters);
   }
 });
+
+function charsInFilm (characters) {
+  for (let i = 0; i < characters.length; i++) {
+    request(characters[i], function (error, response, body) {
+      if (error) {
+        console.error(error);
+      }
+      // Parse string body response to an object
+      if (response.statusCode === 200) {
+        const char = JSON.parse(body);
+        console.log(char.name);
+      }
+    });
+  }
+}
